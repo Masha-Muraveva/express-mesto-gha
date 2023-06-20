d623da5153a6d5f1916c4c092a2db32b6f252769
Masha Muraveva <Maria.Muraveva.S@yandex.ru>
2023-06-20 23:44:07 +0300

    bugfix: реализовать несуществующий роут (после ревью ПР13)

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const usersRouter = require('./users');
 
 router.use('/cards', cardsRouter);
 router.use('/users', usersRouter);
+router.use((req, res) => {
+  res.status(404).send({ message: 'Страница по указанному маршруту не найдена' });
+});
 
 module.exports = router;
