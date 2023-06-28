b9748cde93b693bc01f1213d959601c5946930db
Masha Muraveva <Maria.Muraveva.S@yandex.ru>
2023-06-28 15:10:48 +0300

    bigfix: корректировки по ответу сервера

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const errorHandler = require('./middlewares/errorHandler');
+const { errors } = require('celebrate');
 
+const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes/index');
 const { createUser, login } = require('./controllers/auth');
 
@@ -11,15 +12,17 @@ const app = express();
 mongoose.connect('mongodb://127.0.0.1/mestodb');
 
 app.use(helmet());
-app.disable('x-powered-by');
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/signin', login);
 app.post('/signup', createUser);
 
 app.use('/', router);
 
+app.use(errors());
 app.use(errorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
